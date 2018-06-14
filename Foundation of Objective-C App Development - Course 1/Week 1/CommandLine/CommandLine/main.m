bc260c64bbbc19d3783ab5cfce40417982388475
Unknown <karimabdelazizmansour@gmail.com>
2018-06-14 05:18:41 +0200

    Module 3 Command Line Program

---
+++ Foundation of Objective-C App Development - Course 1/Week 1/CommandLine/CommandLine/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  CommandLine
+//
+//  Created by Graphic on 6/14/18.
+//  Copyright © 2018 Kraim Ebrahem Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int A = 1;
+    int B = 0;
+    
+    if (A == B) {
+        printf("A equals B\n");
+    } else {
+        printf("A does not equals B\n");
+    }
+    
+    return 0;
+}
